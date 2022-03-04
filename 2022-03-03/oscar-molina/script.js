6bee86491458ecee4b19ff51c69dd2f44242f84b
oscar <oscar.molina1617@gmail.com>
2022-03-04 13:41:22 -0600

    cuadreando con una calc

---
+++ 2022-03-03/oscar-molina/script.js
@@ -0,0 +1,71 @@
+const calculator = document.querySelector('.calculator-container')
+const keys = calculator.querySelector('.calculator-grid')
+const previousDisplay = document.querySelector('.previous-operand')
+const display = document.querySelector('.current-operand')
+
+
+
+keys.addEventListener('click', function(event){
+    if (!event.target.closest('button')) return
+
+    const key = event.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+    const {type} = key.dataset
+    const {previousKeyType} = calculator.dataset
+
+    // const {dataset: {type}, textContent: keyValue} = key
+
+    if (type == 'number') {
+        if (displayValue == '0' || previousKeyType == 'operator'){
+            display.textContent = keyValue
+        }
+        else {
+            display.textContent = displayValue + keyValue
+
+        }
+    }
+
+    if (type == 'operator') {
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+        operatorKeys.forEach(function(element){
+            element.dataset.state = ''
+        })
+        key.dataset.state = 'selected'
+
+        calculator.dataset.firstNumber = displayValue
+        calculator.dataset.operator = key.dataset.key
+    }
+
+    if (type == 'equals') {
+        const firstNumber = calculator.dataset.firstNumber
+        const operator = calculator.dataset.operator
+        const secondNumber = displayValue
+        display.textContent = calculate(firstNumber, operator, secondNumber)
+    }
+
+
+    if (type == 'clear') {
+        display.textContent = 0
+        calculator.dataset.previousKeyType = 'clear'
+    }
+
+    calculator.dataset.previousKeyType = type
+
+})
+
+function calculate(firstNumber, operator, secondNumber) {
+    firstNumber = parseInt(firstNumber)
+    secondNumber = parseInt(secondNumber)
+
+    if (operator == 'plus') return firstNumber + secondNumber
+    if (operator == 'minus') return firstNumber - secondNumber
+    if (operator == 'multiply') return firstNumber * secondNumber
+    if (operator == 'divide') return firstNumber / secondNumber
+    if (operator == 'percent') return secondNumber / 100
+    if (operator == 'negative') return secondNumber * -1
+    if (operator == 'clear') return display.textContent(" ")
+
+    if (operator == 'back') return secondNumber.slice(0,-1)
+}
+
