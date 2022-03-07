603b2e6220c3f94d508593b0645521c7023cd24a
marcosva <99751116+marcosva@users.noreply.github.com>
2022-03-07 13:01:27 -0600

    Add files via upload
    add

---
+++ 2022-03-03/Marcos-vanegas/calculator.js
@@ -0,0 +1,121 @@
+const botonNumero = document.querySelectorAll('[data-numero]')
+const botonOperador = document.querySelectorAll('[data-operador]')
+const botonIgual = document.querySelector('[data-igual]')
+const botonBorrarTodo = document.querySelector('[data-borrar-todo]')
+const botonBorrar = document.querySelector('[data-borrar]')
+const textoValorSuperior = document.querySelector('[data-valor-superior]')
+const textoValorInferior = document.querySelector('[data-valor-inferior]')
+
+
+class Calculadora {
+    constructor(textoValorInferior,textoValorSuperior){
+        this.textoValorInferior = textoValorInferior
+        this.textoValorSuperior = textoValorSuperior
+        this.valorInferior = ''
+        this.valorSuperior = ''
+        this.operador = ''
+    }
+
+    agregarNumero(numero){
+    if(numero === '.' && this.valorInferior.includes( '.')) return
+    this.valorInferior = this.valorInferior + numero
+    }
+    imprimirDisplay() {
+        this.textoValorInferior.innerText = this.valorInferior
+        this.textoValorSuperior.innerText = this.valorSuperior + " " + this.operador + " " + this.valorInferior
+    }
+    borrar (){
+        this.valorInferior = this.valorInferior.slice(0,-1)
+    }
+    elegirOperacion(operador) {
+        if(this.valorInferior == '') return
+        if(this.valorSuperior != '') {
+            this.realizarCalculo()
+        }
+        this.operador = operador
+        this.valorSuperior = this.valorInferior
+        this.valorInferior = ''
+    }
+    realizarCalculo() {
+        let resultado
+        let conversionValorSuperior = parseFloat(this.valorSuperior)
+        let conversionValorInferior = parseFloat (this.valorInferior)
+        if(isNaN(conversionValorSuperior) || isNaN(conversionValorInferior)) return
+        switch (this.operador) {
+            case '+':
+            resultado = conversionValorSuperior + conversionValorInferior
+            break
+            case '-':
+            resultado = conversionValorSuperior - conversionValorInferior
+            break
+            case 'x':
+            resultado = conversionValorSuperior * conversionValorInferior
+            break
+            case '÷':
+            resultado = conversionValorSuperior / conversionValorInferior
+            break
+            default: return
+        }
+        
+        this.valorInferior = resultado
+        this.operador = ''
+        this.valorSuperior= ''
+    }
+
+    limpiarPantalla() {
+        this.valorInferior = ''
+        this.valorSuperior = ''
+        this.operador = ''
+
+    }
+}
+
+
+
+const calculadora = new Calculadora (textoValorInferior,textoValorSuperior)
+
+
+
+botonNumero.forEach(boton => {
+    boton.addEventListener('click', () => {
+        calculadora.agregarNumero(boton.innerText)
+        calculadora.imprimirDisplay() 
+    })
+})
+
+botonBorrar.addEventListener('click',() => {
+    calculadora.borrar()
+    calculadora.imprimirDisplay()
+})
+
+botonOperador.forEach(boton => {
+    boton.addEventListener('click', () => {
+        calculadora.elegirOperacion(boton.innerText)
+        calculadora.imprimirDisplay() 
+    })
+})
+botonIgual.addEventListener('click',() => {
+    calculadora.realizarCalculo()
+    calculadora.imprimirDisplay()
+})
+
+botonBorrarTodo.addEventListener('click',() => {
+    calculadora.limpiarPantalla()
+    calculadora.imprimirDisplay()
+})
+
+const darkMood = document.querySelector ('[data-dark]')
+const lightMood = document.querySelector ('[data-light]')
+
+
+lightMood.addEventListener('click' , () => {
+
+    document.querySelector('body').classList.add('light')
+    const buttomNum = document.querySelectorAll('[data-numero]')
+   
+
+    
+
+})
+
+    
