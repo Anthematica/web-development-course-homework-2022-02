9e711f49e6c270a7f5243225ca4d31c66e65f53c
oscar <oscar.molina1617@gmail.com>
2022-03-07 21:08:50 -0600

    resubiendo la galeria

--- 2022-03-01/oscar-molina/scripts.js
+++ 2022-03-01/oscar-molina/scripts.js
@@ -1,5 +1,6 @@
 const artGalleryMayor = document.querySelector('.art-galery-major img')
 const imageSelectors = document.querySelectorAll('.img-container img')
+const links = document.querySelectorAll('.art-categories-options a')
 
 
 imageSelectors.forEach(function(image) {
@@ -12,5 +13,18 @@ imageSelectors.forEach(function(image) {
             }
         
         image.parentElement.classList.add('focused')
+    })
+})
+
+// Añade la clase con estilos a los elementos a
+links.forEach(function(link) {
+    link.addEventListener('click', function(){
+
+        const active = document.querySelector('.active')
+        if (active) {
+            active.classList.remove('active')
+            }
+        
+        link.classList.add('active')
     })
 })
